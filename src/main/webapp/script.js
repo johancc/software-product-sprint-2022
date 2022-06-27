fd6daee5f929d90856b9bc5a45f2fd1cdb17bd7f
tina liu <tl28625@uga.edu>
2022-06-27 02:14:37 -0400

    clear portfolio & add header
    -cleared the portfolio code
    -create initial header/nav bar with search bar
    -

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,18 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
