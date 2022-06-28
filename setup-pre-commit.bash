2da38105757fd09203dcd1c85179d9332be14f4b
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2022-06-28 19:51:10 -0400

    Add precommit hook for checking for exposed private keys

---
+++ setup-pre-commit.bash
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+# Requires: python3, pip3.
+#
+# Installs and configures the pre-submit checks found in .pre-commit-config.yaml
+# If you'd like to customize your pre-submit checks, see the yaml config.
+
+echo 'export PATH=$PATH:~/.local/bin' >>~/.bashrc
+export PATH=$PATH:~/.local/bin
+pip3 install pre-commit
+pre-commit install
