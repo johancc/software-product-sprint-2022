734a3c0725a15a33bab76f68996c64adc899a9c8
Michael <michaelpeters838@yahoo.com>
2022-07-02 22:21:23 +0000

    Finished json file with resources

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
     const greetings =
-        ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+        ['NAACP'];
 
     // Pick a random greeting.
     const greeting = greetings[Math.floor(Math.random() * greetings.length)];
