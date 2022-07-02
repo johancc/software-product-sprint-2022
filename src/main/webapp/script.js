c3b95c787fd8e74c659a0295fe2581c5e784e3db
Michael <michaelpeters838@yahoo.com>
2022-07-02 22:27:59 +0000

    Removed some changes

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
     const greetings =
-        ['NAACP'];
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
     // Pick a random greeting.
     const greeting = greetings[Math.floor(Math.random() * greetings.length)];
