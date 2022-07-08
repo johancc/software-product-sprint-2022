42e4245cbf2e666e059565f27212d498a156176f
zorazrr <zorazrr@gmail.com>
2022-07-08 19:27:28 +0000

    add lon and lat

--- src/main/java/com/google/sps/servlets/resource/UploadResourceServlet.java
+++ src/main/java/com/google/sps/servlets/resource/UploadResourceServlet.java
@@ -8,12 +8,20 @@ import com.google.cloud.datastore.KeyFactory;
 import com.google.cloud.datastore.ListValue;
 import com.google.cloud.datastore.StringValue;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
 
@@ -38,7 +46,34 @@ public class UploadResourceServlet extends HttpServlet {
     String description =
         Jsoup.clean(request.getParameter("description-input"), Safelist.none());
     String[] category = request.getParameterValues("category");
+    Double lat = 0.0, lon = 0.0;
 
+    // Generate Latitude and Longitude based on Zip Code from user
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest req =
+        HttpRequest.newBuilder()
+            .uri(URI.create(
+                "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC7Xskjjq_cfnffdTfXYmyUBkcekfu3xbg&components=postal_code:" +
+                zip + "&result_type=street_address"))
+            .build();
+    try {
+      HttpResponse<String> res =
+          client.send(req, HttpResponse.BodyHandlers.ofString());
+      String resStr = res.body();
+      JSONParser parser = new JSONParser();
+      JSONArray resArr =
+          (JSONArray)((JSONObject)parser.parse(resStr)).get("results");
+      JSONObject resLatLon =
+          (JSONObject)((JSONObject)((JSONObject)resArr.get(0)).get("geometry"))
+              .get("location");
+      lat = new Double(resLatLon.get("lat").toString());
+      lon = new Double(resLatLon.get("lng").toString());
+    } catch (InterruptedException | ParseException e) {
+      response.getWriter().println("Failed to convert Zip Code");
+      e.printStackTrace();
+    }
+
+    // Store resource in Datastore
     Datastore resourceDatastore =
         DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory =
@@ -52,6 +87,8 @@ public class UploadResourceServlet extends HttpServlet {
             .set("zip", zip)
             .set("category", ListValue.of(handleArray(category)))
             .set("description", description)
+            .set("lat", lat)
+            .set("lon", lon)
             .build();
     resourceDatastore.put(resourceEntity);
 
