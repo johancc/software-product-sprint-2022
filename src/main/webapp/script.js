998e61775a201b261dc1c75a74b0165eeab5917f
Michael <michaelpeters838@yahoo.com>
2022-07-12 00:24:17 +0000

    Revised issues in script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,42 +11,47 @@ function loadResources() {
 
 function createResourceElement(newResource) {
   const dashCard = document.createElement("div");
-  // dashCard.className = "row";
   dashCard.className = 'Resource';
 
-  const nameElement = document.createElement("h5");
+  const nameElement = document.createElement('h5');
   nameElement.innerText = newResource.name;
 
-  const phoneElement = document.createElement("li");
+  const phoneElement = document.createElement('li');
   phoneElement.innerText = newResource.phone;
 
-  const emailElement = document.createElement("li");
+  const emailElement = document.createElement('li');
   emailElement.innerText = newResource.email;
 
-  const urlElement = document.createElement("a");
+  const urlElement = document.createElement('a');
   urlElement.innerText = newResource.url;
 
-  const zipElement = document.createElement("li");
+  const zipElement = document.createElement('li');
   zipElement.innerText = newResource.zip;
 
-  const descriptionElement = document.createElement("p");
+  const descriptionElement = document.createElement('p');
   descriptionElement.innerText = newResource.description;
 
-  const categoryElement = document.createElement("li");
+  const categoryElement = document.createElement('li');
   categoryElement.innerText = newResource.category;
 
-  // const topMostDiv = document.createElement("div");
-  // topMostDiv.className = "col-4";
-  // dashCard.appendChild(topMostDiv);
+  const topMostDiv = document.createElement("div");
+  topMostDiv.className = "col-4";
+  dashCard.appendChild(topMostDiv);
 
-  dashCard.innerHTML =
-      `<div class='col-4'>` `<div class="card">` `<div class='card-body'>`;
-  nameElement;
-  descriptionElement`</div>` `<ul class='list-group list-group-flush'>`;
-  phoneElement;
-  emailElement;
-  zipElement;
-  categoryElement`</ul>` `<div class="card-body">`;
-  urlElement`</div>` `</div>` `</div>`;
+  dashCard.innerHTML = `<div class='col-4'>`
+  topMostDiv
+  nameElement
+  descriptionElement
+  `</div>`
+  `<ul class='list-group list-group-flush'>`
+  phoneElement
+  emailElement
+  zipElement
+  categoryElement
+  `</ul>`
+  `<div class="card-body">`
+  urlElement
+  `</div>`
+  `</div>`;
   return dashCard;
 }
