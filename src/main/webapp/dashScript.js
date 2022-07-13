a70a1eb41a53e8f30f8120e63c46d81ff9b69bf6
Michael <michaelpeters838@yahoo.com>
2022-07-13 18:09:46 +0000

    Updated the resource gallery html

--- src/main/webapp/dashScript.js
+++ src/main/webapp/dashScript.js
@@ -1,17 +1,52 @@
-function loadResources() {
-  fetch("/get-all-resources")
-      .then((response) => response.json())
-      .then((resources) => {
-        const resourceListElement = document.getElementById("resource-list");
-        resources.forEach((newResource) => {
-          resourceListElement.appendChild(createResourceElement(newResource));
-        });
-      });
+// function loadResources() {
+//   fetch("/get-all-resources")
+//       .then((response) => response.json())
+//       .then((resources) => {
+//         const resourceListElement = document.getElementById("resource-list");
+//         resources.forEach((newResource) => {
+//           resourceListElement.appendChild(createResourceElement(newResource));
+//         });
+//       });
+// }
+
+function LoadResources() {
+  let resources = [
+    {
+      name : "something",
+      phone : "654654654",
+      email : "something@something.com",
+      url : "www.google.com",
+      zip : "54565",
+      description : "some description",
+      category : [ "Education", "Other" ],
+    },
+    {
+      name : "something else!",
+      phone : "12345678",
+      email : "something@gmail.com",
+      url : "www.maps.google.com",
+      zip : "12345",
+      description : "something else!",
+      category : [ "Other" ],
+    },
+  ];
+
+  const resourceListElement = document.getElementById("resource-list");
+  resources.forEach((newResource) => {
+    resourceListElement.appendChild(createResourceElement(newResource));
+  });
 }
 
 function createResourceElement(newResource) {
-  let {name : resource_name, phone, email, url, zip, description, category} =
-      newResource;
+  let {
+    name : resource_name,
+    phone,
+    email,
+    url,
+    zip,
+    description,
+    category,
+  } = newResource;
 
   dashCard.innerHTML = `
     <div class='col-4' className="Resource"> 
