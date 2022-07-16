ca659a3b4e1f0c2a13ccf926806c17e91aceca62
zorazrr <zorazrr@gmail.com>
2022-07-16 04:38:59 +0000

    initial try

--- src/main/webapp/dashScript.js
+++ src/main/webapp/dashScript.js
@@ -1,4 +1,4 @@
-function loadResources() {
+function loadAllResources() {
   fetch("/all-resources")
     .then((response) => response.json())
     .then((resources) => {
@@ -6,6 +6,33 @@ function loadResources() {
       resources.forEach((newResource) => {
         resourceListElement.appendChild(createResourceElement(newResource));
       });
+    });
+}
+
+function loadZipResources() {
+  const searchParam = document.getElementById("zipCodeEntry").value;
+  fetch(
+    "/zip-resources" +
+      new URLSearchParams({
+        zipCode: searchParam,
+      })
+  )
+    .then((response) => {
+      response.status == 200
+        ? response.json()
+        : (document.getElementById("resource-list").innerHTML =
+            "Something went wrong, please try searching again.");
+      return;
+    })
+    .then((resources) => {
+      const resourceListElement = document.getElementById("resource-list");
+      if (resources.length == 0) {
+        resourceListElement.innerHTML = `Sorry, no nearby resource found. Add new resources <a href="/ResourceForm.html#goToForm">here</a>!`;
+      } else {
+        resources.forEach((newResource) => {
+          resourceListElement.appendChild(createResourceElement(newResource));
+        });
+      }
     });
 }
 
