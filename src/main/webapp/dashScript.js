08bc2da38cddba37c14fbd2808571aeed8fc94a7
Michael <michaelpeters838@yahoo.com>
2022-07-17 18:36:53 +0000

    Implemented Card Search

--- src/main/webapp/dashScript.js
+++ src/main/webapp/dashScript.js
@@ -40,3 +40,17 @@ function createResourceElement(newResource) {
 </div>`;
   return dashCard;
 }
+
+function search_card() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("col-auto mb-3");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "inline";
+    }
+  }
+}
