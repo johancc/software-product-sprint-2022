97db9d83df0a7915bdff2df91e056490e5f6a6d8
Michael <michaelpeters838@yahoo.com>
2022-07-24 02:53:10 +0000

    Improve search by looking at innerText rather than innerHTML

--- src/main/webapp/dashScript.js
+++ src/main/webapp/dashScript.js
@@ -47,7 +47,7 @@ function search_card() {
   let x = document.getElementsByClassName("col-auto mb-3");
 
   for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+    if (!x[i].innerText.toLowerCase().includes(input)) {
       x[i].style.display = "none";
     } else {
       x[i].style.display = "inline";
